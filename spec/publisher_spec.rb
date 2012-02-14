b2577b7b11a83283a2fa2cd9b217af5ea6ef8768
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2012-02-14 13:28:13 +0100

    And update spec.

--- spec/publisher_spec.rb
+++ spec/publisher_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
 describe "Publisher", "publish" do
   
   it "should send data to the dashboard server" do
-    Dboard::Api::Client.should_receive(:post).with("/sources/new_relic", :body => { :data => { :db => "80%" }.to_json }, :timeout => 1000)
+    Dboard::Api::Client.should_receive(:post).with("/sources/new_relic", :body => { :data => { :db => "80%" }.to_json }, :timeout => 10000)
     Dboard::Publisher.publish(:new_relic, { :db => "80%" })
   end
 
