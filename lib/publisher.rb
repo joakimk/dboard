277bc096c6e95031a23573f9b49d5c57ba1fe104
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2012-02-14 13:27:19 +0100

    Timeout publish after 10 seconds. We had a collector lockup for an hour today, could be this.

--- lib/publisher.rb
+++ lib/publisher.rb
@@ -3,7 +3,7 @@ require 'json'
 module Dboard
   class Publisher
     def self.publish(source, data)
-      Api::Client.post("/sources/#{source}", :body => { :data => data.to_json })
+      Api::Client.post("/sources/#{source}", :body => { :data => data.to_json }, :timeout => 10000)
     rescue SocketError => ex
       puts "SocketError: #{ex.message}"
     end
